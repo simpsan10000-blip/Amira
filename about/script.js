0a6795cafb4922a96c92952f4a9f9a35cffb7123
simpsan10000-blip <simpsan10000@gmail.com>
2026-08-31 21:17:35 +0900

    Add files to pages

---
+++ about/script.js
@@ -0,0 +1,13 @@
+﻿const tabs = document.querySelectorAll(".tabs button")
+
+tabs.forEach(button => {
+    const id = button.querySelector("span").id
+    const target = document.getElementById(id.split("-")[0])
+
+    if (!target) return
+
+    button.addEventListener("click", () => {
+        document.querySelector(".tabbed.active")?.classList.remove("active")
+        target.classList.add("active")
+    })
+})
